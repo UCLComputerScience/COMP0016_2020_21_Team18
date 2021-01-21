7971448aa54e07c20fdf48ef5b38d6b0c7aa887b
Jakub Mularski <hello@jakubmularski.pl>
2021-01-21 16:25:17 -0500

    feat(server): new queries added

--- server.js
+++ server.js
@@ -17,28 +17,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 const botName = 'Grace Bot';
 
-const getDrugs = async (name) => {
+const getNode = async (name,wantedNode,returnNode) => {
     const session = driver.session();
     console.log(name);
     try {
         const result = await session.run(
-            "MATCH (patient:Patient{id:$name})-[:HAS_ENCOUNTER]-(encounter:Encounter)-[:HAS_DRUG]-(drug:Drug) RETURN patient,encounter,drug LIMIT 10",
-            { name }
-        )
-
-        console.log(result.records[0]['_fields'][2].properties.description);
-        return [...new Set(result.records.map(row => row['_fields'][2].properties.description))];
-    } finally {
-        await session.close()
-    }
-}
-
-const getConditions = async (name,wantedNode) => {
-    const session = driver.session();
-    console.log(name);
-    try {
-        const result = await session.run(
-            "MATCH (patient:Patient{id:$name})-[:HAS_ENCOUNTER]-(encounter:Encounter)-"+wantedNode+" RETURN patient,encounter,drug LIMIT 10",
+            "MATCH (patient:Patient{id:$name})-[:HAS_ENCOUNTER]-(encounter:Encounter)-"+wantedNode+" RETURN patient,encounter,"+returnNode+" LIMIT 10",
             { name }
         )
 
@@ -52,13 +36,26 @@ const getConditions = async (name,wantedNode) => {
 const getMessage = async (msg) => {
     const prediction = await getPrediction(msg);
     let data;
+    console.log(prediction);
     switch(prediction.prediction) {
         case 'getDrugs':
-            data = await getDrugs(prediction.entities.DB_personName[0][0]);
-            return "This patient took: " + data.join(", ");
+            data = await getNode(prediction.entities.DB_personName[0][0],"[:HAS_DRUG]-(drug:Drug)","drug");
+            return "This patient took: \n" + data.join(", ");
         case 'getConditions':
-            data = await getConditions();
-            return "This patient has following conditions: " + data.join(",");
+            data = await getNode(prediction.entities.DB_personName[0][0],"[:HAS_CONDITION]-(condition:Condition)","condition");
+            return "This patient has: \n" + data.join(", ");
+        case 'getCarePlan':
+            data = await getNode(prediction.entities.DB_personName[0][0],"[:HAS_CARE_PLAN]-(carePlan:CarePlan)","carePlan");
+            return "This patient has: \n" + data.join(", ");
+        case 'getAllergies':
+            data = await getNode(prediction.entities.DB_personName[0][0],"[:HAS_ALLERGY]-(allergy:Allergy)","allergy");
+            return "This patient has: \n" + data.join(", ");
+        case 'getProcedures':
+            data = await getNode(prediction.entities.DB_personName[0][0],"[:HAS_PROCEDURE]-(procedure:Procedure)","procedure");
+            return "This patient has: \n" + data.join(", ");
+        case 'getObservation':
+            data = await getNode(prediction.entities.DB_personName[0][0],"[:HAS_OBSERVATION]-(observation:Observation)","observation");
+            return "This patient has: \n" + data.join(", ");
         default:
             return "Couldn't understand your question."
     }
