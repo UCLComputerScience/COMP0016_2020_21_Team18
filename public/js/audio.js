de57cdab256e9b960684f7427e4590ec2151702e
Jakub Mularski <hello@jakubmularski.pl>
2021-01-21 15:48:53 -0500

    feat(website): search working

--- public/js/audio.js
+++ public/js/audio.js
@@ -2,37 +2,6 @@ const record = $('#record');
 const stop = $('#stop');
 const messageInput = $('#msg');
 
-// if (navigator.mediaDevices) {
-//     navigator.mediaDevices.getUserMedia(constraints)
-//     .then(function (stream) {
-//         var options = { mimeType: "video/webm; codecs=vp9" };
-//         const mediaRecorder = new MediaRecorder(stream, options);
-        
-//         record.onclick = function() {
-//             mediaRecorder.start();
-//             record.addClass("invisible");
-//             stop.removeClass("invisible");
-//             chunks = [];
-//         };
-
-//         stop.onclick = function() {
-//             mediaRecorder.stop();
-//         };
-
-//         mediaRecorder.onstop = function(e) {
-//             const blob = new Blob(chunks, { 'type' : 'audio/ogg; codecs=opus' });
-//             const audioURL = URL.createObjectURL(blob);
-//             audio.src = audioURL;
-
-//             console.log(audioURL);
-//         }
-
-//         mediaRecorder.ondataavailable = function(e) {
-//             chunks.push(e.data);
-//         }
-//     })
-// }
-
 if ('speechSynthesis' in window) {
     const SpeechRecognition = window.webkitSpeechRecognition;
     recognition = new SpeechRecognition();
@@ -67,7 +36,7 @@ if ('speechSynthesis' in window) {
     stop.click(function() {
         recognition.stop();
         record.removeClass("invisible");
-        stop.addClass("invisible")
+        stop.addClass("invisible");
 
     });
 
