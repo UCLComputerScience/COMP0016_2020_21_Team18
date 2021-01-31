7547c5ccb7f7c186a74cd0ce15e4a885d0021052
Jakub Mularski <hello@jakubmularski.pl>
2021-01-31 12:25:19 -0600

    feat(deploy): added deploy configuration

--- server.js
+++ server.js
@@ -93,6 +93,6 @@ io.on('connection', socket => {
     });
 });
 
-const PORT = 3000 || process.env.PORT;
+const PORT = 80 || process.env.PORT;
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
