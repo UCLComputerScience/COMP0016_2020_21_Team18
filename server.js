f7c53754fca2972842bb11d30e98647ac8f4bf01
SheaMagennis <shea.magennis@gmail.com>
2021-02-04 21:27:36 +0000

    Implementing new query

--- server.js
+++ server.js
@@ -37,12 +37,20 @@ const getConditions = async (name,wantedNode) => {
     console.log(name);
     try {
         const result = await session.run(
-            "MATCH (patient:Patient{id:$name})-[:HAS_ENCOUNTER]-(encounter:Encounter)-"+wantedNode+" RETURN patient,encounter,drug LIMIT 10",
-            { name }
-        )
+            //"MATCH (patient:Patient{id:$name})-[:HAS_ENCOUNTER]-(encounter:Encounter)-" + wantedNode + " RETURN patient,encounter," + returnNode + " LIMIT 10",
+            //{ name });
+            "MATCH (p:Patient{id:$name}) " +
+            "MATCH (p)-[:HAS_ENCOUNTER]-(e:Encounter) " +
+            "WHERE apoc.node.degree.in(e, 'NEXT') = 0 " +
+            "MATCH (e)-[:NEXT*0..]->(e2) " +
+            "MATCH (e2)-"+wantedNode+" " +
+            "WHERE "+returnNode+".description IS NOT NULL " +
+            "RETURN e2," + returnNode,{name});
+        //console.log("values: " + (result.records.map(row => row['_fields'][2].properties.description)));
+        //return [...new Set(result.records.map(row => row['_fields'][2].properties.description))];
+        console.log("values: " + (result.records.map(row => row['_fields'][1].properties.description)));
+        return [...new Set(result.records.map(row => row['_fields'][1].properties.description))];
 
-        console.log(result.records[0]['_fields'][2].properties.description);
-        return [...new Set(result.records.map(row => row['_fields'][2].properties.description))];
     } finally {
         await session.close()
     }
