a98073f30d23d1fa8857cecb11fa1e9ad3d7321c
Jakub Mularski <hello@jakubmularski.pl>
2021-03-29 00:26:23 -0600

    feat(test): added integration test

--- tests/integration/socket.spec.js
+++ tests/integration/socket.spec.js
@@ -1,34 +1,21 @@
-const app = require("../../src/app");
-const io = require("socket.io");
+const io = require("socket.io-client");
 const ioOptions = { 
     transports: ['websocket'],
     forceNew: true, 
     reconnection: false
 };
+
 const data = {
     "message": "what patients have the condition viral sinusitis (disorder)?",
     "response": "This patients with this condition are: Mr.Aaron697 Brekke496"
 }
 
-
 describe('Chat Events', () => {
-    var sender, receiver;
-
-    beforeEach(async done => {
-      sender = io('http://localhost:3001/', ioOptions);
-      receiver = io('http://localhost:3001/', ioOptions);
-      
-      done();
-    });
-
-    afterEach(done => {
-      sender.disconnect();
-      receiver.disconnect();
-      
-      done();
-    });
 
     it('client should receive a correct answer', done => {
+      const sender = io('http://localhost:3001/', ioOptions);
+      const receiver = io('http://localhost:3001/', ioOptions);
+
       receiver.emit('chatMessage', data.message);
 
       sender.on('chatMessage', () => {
@@ -37,8 +24,11 @@ describe('Chat Events', () => {
 
       receiver.on('message', (msg) => {
         expect(msg).to.equal(data.response);
-        done();
       });
+
+      sender.disconnect();
+      receiver.disconnect();
+      done();
     });
 
 });
